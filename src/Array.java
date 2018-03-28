f20bdb8c97becbd4844d109ad80ffbdbc634b7f7
yukerui <ykrui1995@gmail.com>
2018-03-28 16:36:17 +0800

    Initial commit

---
+++ src/Array.java
@@ -0,0 +1,9 @@
+public class Array {
+    public static void main(String[]args){
+        int[] anonymous = {17,19,23,29,31,37};
+        int []luckNumbers = anonymous;
+        System.out.println(luckNumbers[2]);
+
+
+    }
+}
